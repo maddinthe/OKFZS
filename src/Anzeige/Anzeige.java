c494d589c91dec8a4097659578c2a4e575ce61a7
maddinthe <martintheilen@gmail.com>
2016-05-16 11:44:05 +0200

    Initial commit

---
+++ src/Anzeige/Anzeige.java
@@ -0,0 +1,7 @@
+package Anzeige;
+
+/**
+ * Created by Martin on 16.05.2016.
+ */
+public class Anzeige {
+}
---
+++ src/Anzeige/Werkzeug/PersonEditor.java
@@ -0,0 +1,7 @@
+package Anzeige.Werkzeug;
+
+/**
+ * Created by Martin on 16.05.2016.
+ */
+public class PersonEditor {
+}
