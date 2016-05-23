6a51d273c947bf7a45ff5627aca42eb16dde09d8
maddinthe <martintheilen@gmail.com>
2016-05-23 15:36:03 +0200

    erste schritte 3

--- src/GUI/Uebersicht.java
+++ src/GUI/Uebersicht.java
@@ -2,6 +2,7 @@ package GUI;
 
 import Datenhaltung.Vorgang;
 
+import javax.swing.*;
 import java.util.List;
 
 /**
@@ -13,6 +14,23 @@ public class Uebersicht extends Ansicht {
     //todo: unfertig und doku
     public Uebersicht(OKFZS okfzsInstanz,List<Vorgang> vorgaenge){
         super(okfzsInstanz);
+
+        String[][] td={
+                {"Meyer, Hans","Nissan Micra Cool","9.500,00€","03.05.16"},
+                {"Müller, Ingo","VW Golf Tour","19.500,00€","02.05.16"},
+                {"Michael, Rudi","Toyota Aygo Basic","6.800,00€","02.05.16"},
+                {"Schmidt, Dennis","Skoda Octavia Elegance","18.520,00€","01.05.16"}
+        };
+        String[] head={"Käufer","Auto","Preis","Verkaufsdatum"};
+
+
+        JTable tabelle=new JTable(td,head);
+        tabelle.setFont(tabelle.getFont().deriveFont(20));
+
+        this.add(new JScrollPane(tabelle));
+
+
+
 
 
     }
--- src/testwiese/MaddinTest.java
+++ src/testwiese/MaddinTest.java
@@ -1,10 +1,30 @@
 package testwiese;
 
+import Datenhaltung.KFZ;
+import Datenhaltung.Vorgang;
+import GUI.Menue;
+import GUI.OKFZS;
+import GUI.Uebersicht;
+
+import javax.swing.*;
+import java.util.LinkedList;
+
 /**
  * Created by mtheilen on 23.05.2016.
  */
 public class MaddinTest {
     public static void main(String[] args) {
+
+
+
+
+        JFrame test=new JFrame("OKFZS-TEST-Maddin");
+        test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        test.setSize(1024, 786);
+        test.setJMenuBar(new Menue(null));
+        test.add(new Uebersicht(null,new LinkedList<Vorgang>()));
+
+        test.setVisible(true);
 
     }
 }
