4bcb629a86e289547fbe7fe7b05106fb231156b0
Fresh-87 <dreher-87@web.de>
2016-05-23 09:04:55 +0200

    Methodenköpfe

---
+++ src/GUI/Anmeldung.java
@@ -0,0 +1,12 @@
+package GUI;
+
+import Datenhaltung.Verkaeufer;
+
+/**
+ * Created by cdreher on 23.05.2016.
+ */
+public class Anmeldung extends Ansicht {
+    public Verkaeufer anmelden(String name,String passwort){
+        return Verkaeufer;
+    }
+}
---
+++ src/GUI/Werkzeug/Suche.java
@@ -0,0 +1,22 @@
+package GUI.Werkzeug;
+
+
+
+
+/**
+ * Created by cdreher on 23.05.2016.
+ */
+public class Suche {
+    public Suche(String spalte, String begriff, boolean erfolg) {
+        this.spalte=spalte;
+        this.begriff=begriff;
+        this.erfolg=erfolg;
+    }
+
+    private void sucheKFZ(String name, String begriff){
+
+    }
+    private void suchePerson(String name, String begriff){
+
+    }
+}
