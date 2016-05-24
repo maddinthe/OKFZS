6e925cfd401340ebc861b81275c459fbc0325e28
Fresh-87 <dreher-87@web.de>
2016-05-24 12:07:23 +0200

    Datenbank Insert müsste laufen

--- src/GUI/Werkzeug/PersonenEditor.java
+++ src/GUI/Werkzeug/PersonenEditor.java
@@ -6,6 +6,7 @@ import GUI.Ansicht;
 import GUI.OKFZS;
 
 import java.sql.SQLException;
+import java.util.Date;
 
 /**
  * Created by cdreher on 16.05.2016.
@@ -19,12 +20,12 @@ public class PersonenEditor extends Ansicht {
 
 
     }
-    public PersonenEditor(OKFZS okfzsInstanz,String anrede, String name) throws SQLException {
+    public PersonenEditor(OKFZS okfzsInstanz,String anrede, String name,Date geburtstag) throws SQLException {
         super(okfzsInstanz);
-        System.out.println(new Person(anrede, name));
+        System.out.println(new Person(anrede, name,geburtstag));
        Datenbank db = okfzsInstanz.getDatenbank();
         try {
-            db.insertOrUpdatePerson(anrede, name);
+            db.insertOrUpdatePerson(new Person(anrede,name,geburtstag));
             db.printTable("t_Person");
         } catch (SQLException e1) {
             System.out.println(e1.getMessage());
--- src/testwiese/TurnerTest.java
+++ src/testwiese/TurnerTest.java
@@ -4,6 +4,9 @@ import GUI.OKFZS;
 import GUI.Werkzeug.PersonenEditor;
 
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by cdreher on 23.05.2016.
@@ -11,8 +14,20 @@ import java.sql.SQLException;
 public class TurnerTest {
     public static void main(String[] args) throws SQLException {
         OKFZS okfzs = new OKFZS();
-        PersonenEditor personenEditor =new PersonenEditor(okfzs,"Herr","Theilen");
+        PersonenEditor personenEditor =new PersonenEditor(okfzs,"Herr","Dreher",umwandeln("13.01.1987"));
 
+    }
+    public static Date umwandeln(String datum) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = null;
+        try {
+            date = format.parse(datum);
+            System.out.println(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+
+        }
+        return date;
     }
 
 
