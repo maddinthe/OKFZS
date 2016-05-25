f2e7a385345faa7d18701c90d8600eaa45b1f896
tonikertz <tonikertz@googlemail.com>
2016-05-25 09:59:22 +0200

    Hilfe angefangen

--- src/GUI/Statistik.java
+++ src/GUI/Statistik.java
@@ -10,29 +10,27 @@ import java.util.List;
 /**
  * Created by tkertz on 23.05.2016.
  */
-public class Statistik extends Ansicht {
-
-    private Person person;
-    private Vorgang vorgang;
-    private Verkaeufer verkaeufer;
-
-    public Statistik(List<Vorgang> vorgangList, OKFZS okfzsInstanz) {
-        super(okfzsInstanz);
-        okfzsInstanz.getDatenbank().VorgaengeZuVerkaeufer(okfzsInstanz.getDatenbank().einVerkaufer(okfzsInstanz.getBenutzer().getPerson().getPid()));
-
-    }
-
-    public void einzelVerkaeuferStatistik(OKFZS okfzsInstanz2) {
-
-        okfzsInstanz.getDatenbank().VorgaengeZuVerkaeufer(okfzsInstanz.getDatenbank().einVerkaufer(okfzsInstanz.getBenutzer().getPerson().getPid()));
-    }
-
-    public void gesamtVerkaeuferStatistik() {
-
-
-    }
-
-    public static void main(String[] args) {
-
-    }
-}
+//public class Statistik extends Ansicht {
+//
+//    private Person person;
+//    private Vorgang vorgang;
+//    private Verkaeufer verkaeufer;
+//
+//    public Statistik(List<Vorgang> vorgangList, OKFZS okfzsInstanz) {
+//        super(okfzsInstanz);
+//        okfzsInstanz.getDatenbank().VorgaengeZuVerkaeufer(okfzsInstanz.getDatenbank().einVerkaufer(okfzsInstanz.getBenutzer().getPerson().getPid()));
+//
+//    }
+//
+//    public void einzelVerkaeuferStatistik(OKFZS okfzsInstanz) {
+//       vorgang = okfzsInstanz.getDatenbank().VorgaengeZuVerkaeufer(okfzsInstanz.getDatenbank().einVerkaufer(okfzsInstanz.getBenutzer().getPerson().getPid()));
+//       vorgang.getGewinn(okfzsInstanz.getDatenbank().einVerkaufer(okfzsInstanz.getBenutzer().getPerson().getPid()))
+//        for(Vorgang v: vorgangList)
+//    }
+//
+//    public void gesamtVerkaeuferStatistik() {
+//
+//
+//    }
+//
+//}
