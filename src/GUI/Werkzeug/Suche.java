1ee533a59182c6d94501a90621e322afc70f5664
Fresh-87 <dreher-87@web.de>
2016-05-25 11:24:58 +0200

    Suche implementiert

--- src/GUI/Werkzeug/Suche.java
+++ src/GUI/Werkzeug/Suche.java
@@ -15,62 +15,152 @@ import java.util.List;
 public class Suche {
     OKFZS okfzsinstanz = new OKFZS();
 
-    public Suche(String spalte, String begriff, boolean entscheidung) {
-
+    public Suche(String spalte, String begriff, boolean kfzOderPerson) {
+        if (kfzOderPerson)
+            sucheKFZ(spalte, begriff);
+        else
+            suchePerson(spalte, begriff);
     }
 
-    private void sucheKFZ(String name, String begriff){
+    private KFZ sucheKFZ(String spalte, String begriff) {
+
         try {
             List<KFZ> kfzListe = okfzsinstanz.getDatenbank().alleKfz();
+            switch (spalte) {
+                case ("Fin"):
+                    for (KFZ kfz : kfzListe) {
+                        if (kfz.getFin().equals(begriff))
+                            return kfz;
+                    }
+                    break;
+                case ("Hersteller"):
+                    for (KFZ kfz : kfzListe) {
+                        if (kfz.getHersteller().equals(begriff))
+                            return kfz;
+                    }
+                    break;
+                case ("Modell"):
+                    for (KFZ kfz : kfzListe) {
+                        if (kfz.getModell().equals(begriff))
+                            return kfz;
+                    }
+                    break;
+                case ("KFZ-Brief"):
+                    for (KFZ kfz : kfzListe) {
+                        if (kfz.getKfzBriefNr().equals(begriff))
+                            return kfz;
+                    }
+                    break;
+                case ("Leistung"):
+                    for (KFZ kfz : kfzListe) {
+                        if (kfz.getLeistungInKw() == (Integer.parseInt(begriff)))
+                            return kfz;
+                    }
+                    break;
+                case ("Farbe"):
+                    for (KFZ kfz : kfzListe) {
+                        if (kfz.getFarbe().equals(begriff))
+                            return kfz;
+                    }
+                    break;
+                case ("EZ"):
+                    for (KFZ kfz : kfzListe) {
+                        if (kfz.getEz().toString().equals(begriff))
+                            return kfz;
+                    }
+                    break;
+                case ("Plakette"):
+                    for (KFZ kfz : kfzListe) {
+                        if (kfz.getUmweltPlakette() == (Byte.parseByte(begriff)))
+                            return kfz;
+                    }
+                    break;
+                case ("Kraftstoff"):
+                    for (KFZ kfz : kfzListe) {
+                        if (kfz.getKraftstoff().equals(begriff))
+                            return kfz;
+                    }
+                    break;
+                default:
+                    System.out.println("Fehler");
+            }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return null;
     }
-    private void suchePerson(String name, String begriff){
+
+    private Person suchePerson(String spalte, String begriff) {
+
+        List<Person> personenListe = null;
         try {
-            List<Person> personenListe = okfzsinstanz.getDatenbank().allePersonen();
+            personenListe = okfzsinstanz.getDatenbank().allePersonen();
+
+            switch (spalte) {
+                case ("Pid"):
+                    for (Person person : personenListe) {
+                        if (person.getPid() == (Long.parseLong(begriff)))
+                            return person;
+                    }
+                    break;
+                case ("Anrede"):
+                    for (Person person : personenListe) {
+                        if (person.getAnrede().equals(begriff))
+                            return person;
+                    }
+                    break;
+                case ("Name"):
+                    for (Person person : personenListe) {
+                        if (person.getName().equals(begriff))
+                            return person;
+                    }
+                    break;
+                case ("Vorname"):
+                    for (Person person : personenListe) {
+                        if (person.getVorname().equals(begriff))
+                            return person;
+                    }
+                    break;
+                case ("Geburtstag"):
+                    for (Person person : personenListe) {
+                        if (person.getGeburtstag().toString().equals(begriff))
+                            return person;
+                    }
+                    break;
+                case ("Anschrift"):
+                    for (Person person : personenListe) {
+                        if (person.getAnschrift().equals(begriff))
+                            return person;
+                    }
+                    break;
+                case ("PLZ"):
+                    for (Person person : personenListe) {
+                        if (person.getPostleitzahl() == (Integer.parseInt(begriff)))
+                            return person;
+                    }
+                    break;
+                case ("Ort"):
+                    for (Person person : personenListe) {
+                        if (person.getOrt().equals(begriff))
+                            return person;
+                    }
+                    break;
+                case ("Ust-ID"):
+                    for (Person person : personenListe) {
+                        if (person.getUstID().equals(begriff))
+                            return person;
+                    }
+                    break;
+                default:
+                    System.out.println("Fehler");
+
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return null;
     }
 
 
-
-
-
-
-
-
-
-    switch (suche) {
-        case "h":
-            for (KFZ kfz : kfzliste) {
-                if (kfz.getHersteller().equals(parameter))
-                    System.out.println(kfz.toString());
-            }
-            break;
-        case "m":
-            for (KFZ kfz : kfzliste) {
-                if (kfz.getModell().equals(parameter))
-                    System.out.println(kfz.toString());
-            }
-            break;
-        case "p":
-            for (KFZ kfz : kfzliste) {
-                if (kfz.getPreis().equals(parameter))
-                    System.out.println(kfz.toString());
-            }
-            break;
-        case "hmp":
-            for (KFZ kfz : kfzliste) {
-                if (kfz.getHersteller().equals(parameter) || kfz.getModell().equals(parameter) || kfz.getPreis().equals(parameter))
-                    System.out.println(kfz.toString());
-            }
-            break;
-        default:
-            System.out.println("Fehler");
-    }
-
-
-}
 }
