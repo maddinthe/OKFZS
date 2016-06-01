e077d0825a02a0814e38866c711ac3ca9b5acb9f
tonikertz <tonikertz@googlemail.com>
2016-06-01 12:18:01 +0200

    PersEditor PersÄndern Layout Änderungen

--- src/GUI/Werkzeug/AktionEditor.java
+++ src/GUI/Werkzeug/AktionEditor.java
@@ -17,20 +17,33 @@ import java.util.*;
 import java.util.List;
 
 /**
- * Created by tkertz on 23.05.2016.
+ * Created by  on 23.05.2016.
+ * Der Aktioneneditor erweitert die Ansicht und stellt die Möglichkeit bereit, eine Fahrzeugaktion
+ * z.B. Probefahrt hinzuzufügen
+ * @author tkertz
  */
 public class AktionEditor extends Ansicht {
 
+    /**
+     * Dieser Aktionenkonstruktor stellt die Möglichkeit zur Verfügung für eine übergebene Instanz nebst KFZ eine Aktion
+     * zu erstellen
+     * @param okfzsInstanz
+     * @param kfz
+     */
     public AktionEditor(OKFZS okfzsInstanz, KFZ kfz) {
         super(okfzsInstanz);
 
-              //      KFZ DATEN
+        /*
+         * Panel für die Ansicht als Popup Fenster wird generiert
+         */
+
         JFrame jfAktionEdit = new JFrame("Aktionen-Editor");
         JPanel jpKFZ = new JPanel();
         jpKFZ.setLayout(new BoxLayout(jpKFZ, BoxLayout.Y_AXIS));
 
-
-        //Aktion
+        /*
+         * Panelrahmen und Beschriftung im JFrame Popup
+         */
 
         JPanel jpAktion = new JPanel();
         jpAktion.setLayout(new BoxLayout(jpAktion, BoxLayout.Y_AXIS));
@@ -39,16 +52,26 @@ public class AktionEditor extends Ansicht {
         jpAktionen.setBorder(new TitledBorder("Aktionen"));
         jpAktionen.setLayout(new BoxLayout(jpAktionen, BoxLayout.Y_AXIS));
 
+        /*
+         * Panel für Datumseingabe und Beschriftung über ein Label
+         */
         JPanel jpDatumAktion = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         JLabel jlDatumAktion = new JLabel("Datum: * ");
         JTextField jtDatumAktion = new JTextField(20);
         jpDatumAktion.add(jlDatumAktion);
         jpDatumAktion.add(jtDatumAktion);
 
+        /*
+         * Dropdownmenü für den Durchführenden der Aktion. Der Durchführende kann ein potentieller Käufer sein, oder
+         * eben eine unternehmensnahe Person, wie z.B. Mechaniker, Aufbereiter etc. pp.
+         */
         JPanel jpDurchfuehrender = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         JLabel jlDurchfuehrender = new JLabel("Durchführender: * ");
         Person[] persons=null;
 
+        /*
+         * Personenliste (Inhalt)für das Dropdownmenü erstellt und an die JComboBox übergebne
+         */
         try {
             List<Person> personList=okfzsInstanz.getDatenbank().allePersonenSortiert();
             persons=personList.toArray(new Person[personList.size()]);
@@ -73,7 +96,9 @@ public class AktionEditor extends Ansicht {
 
         jpAktion.add(jpAktionen);
 
-//        Actionlistener
+        /*
+          Actionlistener der die erstellte Aktion an die DB übertragt
+         */
 
         ActionListener alAktionSpeichern = new ActionListener() {
             @Override
@@ -94,8 +119,9 @@ public class AktionEditor extends Ansicht {
             }
         };
 
-
-        //        Button
+        /*
+          Speichern-Button und Vervollständigung
+         */
 
         JButton jbAktionSave = new JButton("Speichern");
         jbAktionSave.addActionListener(alAktionSpeichern);
@@ -120,187 +146,14 @@ public class AktionEditor extends Ansicht {
         jfAktionEdit.setVisible(true);
 
 
-
-
     }
 
-//    public AktionEditor(KFZ kfz, OKFZS okfzsInstanz) {
-//        super(okfzsInstanz);
-//        //      KFZ DATEN
-//
-//        JFrame jfAktionEdit = new JFrame("Aktionen-Editor");
-//        JPanel jpKFZ = new JPanel();
-//        jpKFZ.setLayout(new BoxLayout(jpKFZ, BoxLayout.Y_AXIS));
-//
-//        JPanel jpKfzAngaben = new JPanel();
-//        jpKfzAngaben.setBorder(new TitledBorder("KFZ-Angaben"));
-//        jpKfzAngaben.setLayout(new BoxLayout(jpKfzAngaben, BoxLayout.Y_AXIS));
-//
-//        JPanel jpTypAngaben = new JPanel();
-//        jpTypAngaben.setBorder(new TitledBorder("Typ-Angaben"));
-//        jpTypAngaben.setLayout(new BoxLayout(jpTypAngaben, BoxLayout.Y_AXIS));
-//
-//        JPanel jpSonstigeAngaben = new JPanel();
-//        jpSonstigeAngaben.setBorder(new TitledBorder("Sonstige Angaben"));
-//        jpSonstigeAngaben.setLayout(new BoxLayout(jpSonstigeAngaben, BoxLayout.Y_AXIS));
-//
-//        JPanel jpFin = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-//        JLabel jlFin = new JLabel("Fin:");
-//        JTextField jtFin = new JTextField(20);
-//        jtFin.setText(kfz.getFin());
-//        jtFin.setEditable(false);
-//        jpFin.add(jlFin);
-//        jpFin.add(jtFin);
-//
-//        JPanel jpHersteller = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-//        JLabel jlHersteller = new JLabel("Hersteller:");
-//        JTextField jtHersteller = new JTextField(20);
-//        jtHersteller.setText(kfz.getHersteller());
-//        jtHersteller.setEditable(false);
-//        jpHersteller.add(jlHersteller);
-//        jpHersteller.add(jtHersteller);
-//
-//        JPanel jpModell = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-//        JLabel jlModell = new JLabel("Modell:");
-//        JTextField jtModell = new JTextField(20);
-//        jtModell.setText(kfz.getModell());
-//        jtModell.setEditable(false);
-//        jpModell.add(jlModell);
-//        jpModell.add(jtModell);
-//
-//        JPanel jpKfzBriefNr = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-//        JLabel jlKfzBriefNr = new JLabel("KFZ-Brief-Nr.:");
-//        JTextField jtKfzBriefNr = new JTextField(20);
-//        jtKfzBriefNr.setText(kfz.getKfzBriefNr());
-//        jtKfzBriefNr.setEditable(false);
-//        jpKfzBriefNr.add(jlKfzBriefNr);
-//        jpKfzBriefNr.add(jtKfzBriefNr);
-//
-//        JPanel jpLeistungInKw = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-//        JLabel jlLeistungInKw = new JLabel("Leistung in KW:");
-//        JTextField jtLeistungInKw = new JTextField(20);
-//        jtLeistungInKw.setText(String.valueOf(kfz.getLeistungInKw()));
-//        jtLeistungInKw.setEditable(false);
-//        jpLeistungInKw.add(jlLeistungInKw);
-//        jpLeistungInKw.add(jtLeistungInKw);
-//
-//        JPanel jpFarbe = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-//        JLabel jlFarbe = new JLabel("Farbe:");
-//        JTextField jtFarbe = new JTextField(20);
-//        jtFarbe.setText(kfz.getFarbe());
-//        jtFarbe.setEditable(false);
-//        jpFarbe.add(jlFarbe);
-//        jpFarbe.add(jtFarbe);
-//
-//        JPanel jpEZ = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-//        JLabel jlEZ = new JLabel("Erstzulassung:");
-//        JTextField jtEZ = new JTextField(20);
-//        jtEZ.setText(String.valueOf(kfz.getEz()));
-//        jtEZ.setEditable(false);
-//        jpEZ.add(jlEZ);
-//        jpEZ.add(jtEZ);
-//
-//        JPanel jpUmweltplakette = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-//        JLabel jlUmweltplakette = new JLabel("Umweltplakette:");
-//        JTextField jtUmweltplakette = new JTextField(20);
-//        jtUmweltplakette.setText(String.valueOf(kfz.getUmweltPlakette()));
-//        jtUmweltplakette.setEditable(false);
-//        jpUmweltplakette.add(jlUmweltplakette);
-//        jpUmweltplakette.add(jtUmweltplakette);
-//
-//        JPanel jpKraftstoff = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-//        JLabel jlKraftstoff = new JLabel("Kraftstoff:");
-//        JTextField jtKraftstoff = new JTextField(20);
-//        jtKraftstoff.setText(kfz.getKraftstoff());
-//        jtKraftstoff.setEditable(false);
-//        jpKraftstoff.add(jlKraftstoff);
-//        jpKraftstoff.add(jtKraftstoff);
-//
-//
-//        JPanel jpSonderausstattungen = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-//        JLabel jlSonderausstattungen = new JLabel("Sonderausstattung:");
-//        JTextField jtSonderausstattungen = new JTextField(20);
-//        String[] ausstattung = {"Leder", "Navigation", "PDC", "Xenon", "Tempomat"};
-//        JComboBox jcAusstattungListe = new JComboBox(ausstattung);
-//        jcAusstattungListe.setSelectedItem(kfz.getSonderausstattung());
-//        jcAusstattungListe.setEditable(false);
-//        jcAusstattungListe.setSelectedIndex(4);
-//        jcAusstattungListe.setEditable(true);
-//
-//        jpSonderausstattungen.add(jlSonderausstattungen);
-//        jpSonderausstattungen.add(jcAusstattungListe);
-//
-//        jpKfzAngaben.add(jpFin);
-//        jpKfzAngaben.add(jpKfzBriefNr);
-//        jpKfzAngaben.add(jpEZ);
-//        jpKfzAngaben.add(jpLeistungInKw);
-//
-//        jpTypAngaben.add(jpHersteller);
-//        jpTypAngaben.add(jpModell);
-//        jpTypAngaben.add(jpFarbe);
-//        jpTypAngaben.add(jpSonderausstattungen);
-//
-//        jpSonstigeAngaben.add(jpKraftstoff);
-//        jpSonstigeAngaben.add(jpUmweltplakette);
-//
-//
-//        jpKFZ.add(jpTypAngaben);
-//        jpKFZ.add(jpKfzAngaben);
-//        jpKFZ.add(jpSonstigeAngaben);
-//
-//        //Aktion
-//
-//        JPanel jpAktion = new JPanel();
-//        jpAktion.setLayout(new BoxLayout(jpAktion, BoxLayout.Y_AXIS));
-//
-//        JPanel jpAktionen = new JPanel();
-//        jpAktionen.setBorder(new TitledBorder("Aktionen"));
-//        jpAktionen.setLayout(new BoxLayout(jpAktionen, BoxLayout.Y_AXIS));
-//
-//        JPanel jpDatumAktion = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-//        JLabel jlSchaeden = new JLabel("Datum:");
-//        JTextField jtSchaeden = new JTextField(20);
-//        jpDatumAktion.add(jlSchaeden);
-//        jpDatumAktion.add(jtSchaeden);
-//
-//        JPanel jpDurchfuehrender = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-//        JLabel jlDurchfuehrender = new JLabel("Durchführender:");
-//        JTextField jtDurchfuehrender = new JTextField(20);
-//        jpDurchfuehrender.add(jlDurchfuehrender);
-//        jpDurchfuehrender.add(jtDurchfuehrender);
-//
-//        JPanel jpBeschreibung = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-//        JLabel jlKennzeichen = new JLabel("Beschreibung:");
-//        JTextArea jtKennzeichen = new JTextArea(20, 20);
-//
-//        jpBeschreibung.add(jlKennzeichen);
-//        jpBeschreibung.add(jtKennzeichen);
-//
-//        jpAktionen.add(jpDatumAktion);
-//        jpAktionen.add(jpDurchfuehrender);
-//        jpAktionen.add(jpBeschreibung);
-//
-//        jpAktion.add(jpAktionen);
-//
-//        jfAktionEdit.add(jpKFZ, BorderLayout.WEST);
-//        jfAktionEdit.add(jpAktion, BorderLayout.EAST);
-//
-//
-//        //JFrame jf Größe mitgeben
-//        jfAktionEdit.setSize(1024, 768);
-//
-//
-//        //JFrame jf auf Bildschirm plazieren
-//        jfAktionEdit.setLocation(200, 400);
-//
-//        //JFrame jf, beim Klicken auf X ist Fenster nicht sichtbar, Programm wird erst geschlossen wenn alle geschlossen sind
-//        jfAktionEdit.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-//
-//        //JFrame jf anzeigen
-//        jfAktionEdit.setVisible(true);
-//
-//
-//    }
+
+    /**
+     * Aktioneneditor der die Möglichkeit bietet eine bestehende Aktion zu einem Fahrzeug zu bearbeiten
+     * @param aktion
+     * @param okfzsInstanz
+     */
 
     public AktionEditor(Aktion aktion, OKFZS okfzsInstanz) {
         super(okfzsInstanz);
@@ -376,8 +229,6 @@ public class AktionEditor extends Ansicht {
         };
 
 
-
-
         //        Button
 
         JButton jbAktionSave = new JButton("Speichern");
@@ -406,11 +257,9 @@ public class AktionEditor extends Ansicht {
 
     }
 
-
-
-//    public Aktion getAktion() {
-//        return Aktion();
-//    }
+    /**
+     * Methode die das eigentliche Popup Fenster generiert und die Eingaben: Datum, Durchführender und Beschreibung ermöglicht
+     */
 
     public void setNeueAktion(){
         JFrame jfKfzEdit = new JFrame("Ausstattungs-Editor");
